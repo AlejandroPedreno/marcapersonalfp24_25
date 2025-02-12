5479c9c52fda5d450435d0fa07067d9771f6b30f
Alejandro Pedreño <5589188@alu.murciaeduca.es>
2025-02-12 08:26:09 +0100

    Autenticando con tokens

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'getHome']);
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
 });
-
-include __DIR__.'/actividades.php';
-include __DIR__.'/ciclos.php';
-include __DIR__.'/curriculos.php';
-include __DIR__.'/familias_profesionales.php';
-include __DIR__.'/proyectos.php';
-include __DIR__.'/reconocimientos.php';
-include __DIR__.'/users.php';
 
 require __DIR__.'/auth.php';
